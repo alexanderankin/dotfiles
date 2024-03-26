2b9908504ad8162f355bb99fa488757d383d4967
David Ankin <alexanderankin@gmail.com>
2024-03-25 21:08:06 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [[ "$0" != "$BASH_SOURCE" ]]; then echo "no sourcing">&2; return 1; fi;
 set -eu -o pipefail
 [[ ${DEBUG:-} ]] && set -x
 
-full="$BASH_SOURCE"; dir=${full%\/*}; file=${full##*/};
+full="$(readlink -f "$BASH_SOURCE")"; dir=${full%\/*}; file=${full##*/};
 
 files=""
 for i in ~/.bashrc ~/.bash_aliases ~/.profile ; do files="$files $i"; done; unset i;
@@ -11,6 +11,6 @@ for i in ~/.bashrc ~/.bash_aliases ~/.profile ; do files="$files $i"; done; unse
 backup=$(date +%Y-%m-%d)
 
 for f in $files; do
-  mv $f{,.backup.$backup};
+  mv -vn $f{,.backup.$backup};
   ln -s $dir/$(basename $f) $f;
 done
